990d8cd447e413790e9348709c3a8dadd60e36a8
Matheus da Silva Dantas <20222014040018@ifrn.local>
2023-07-12 14:02:49 -0300

    Teste apagar

--- MainPlayground/Modelo/novo.cs
+++
@@ -1,9 +0,0 @@
-
-class Crianca{
-    public int id { get; set}
-    public string nome { get; set}
-    public int idade { get; set}
-    public string genero { get; set}
-    public string outros { get; set}
-}
-
