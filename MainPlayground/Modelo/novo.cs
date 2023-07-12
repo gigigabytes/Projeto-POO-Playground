845ece521e8fcd8873f3681240139c4bd21aefa0
Maria Giovanna Lino dos Santos <20211014040040@ifrn.local>
2023-07-12 14:01:04 -0300

    teste novo

---
+++ MainPlayground/Modelo/novo.cs
@@ -0,0 +1,9 @@
+
+class Crianca{
+    public int id { get; set}
+    public string nome { get; set}
+    public int idade { get; set}
+    public string genero { get; set}
+    public string outros { get; set}
+}
+
